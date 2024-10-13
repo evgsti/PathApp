e8717df255966c44e18f7c22ea91a39317e66d75
Evgeniy <ev.shipitsyn@yandex.ru>
2024-10-13 15:01:38 +0300

    Initial Commit

---
+++ PathApp/PathAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathAppApp.swift
+//  PathApp
+//
+//  Created by Евгений on 13.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct PathAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
