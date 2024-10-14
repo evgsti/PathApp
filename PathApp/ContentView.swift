5d5914eda89d0bc35c3ed18b8b9b79c242d99e2d
Evgeniy <ev.shipitsyn@yandex.ru>
2024-10-14 09:19:35 +0300

    work with UI

--- PathApp/ContentView.swift
+++ PathApp/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     let size: CGFloat = 300
     
     @State private var rotationAngle: Angle = .zero
+    @State private var scale: CGFloat = .zero
     
     var body: some View {
         ZStack {
@@ -20,36 +21,51 @@ struct ContentView: View {
                     .frame(width: size, height: size)
                     .rotationEffect(rotationAngle)
                     .offset(x: -82, y: 191.5)
+                    .scaleEffect(scale)
+                    .animation(.easeInOut(duration: 2), value: scale)
                 
                 GearView()
                     .foregroundStyle(.red)
                     .frame(width: size, height: size)
                     .rotationEffect(-rotationAngle - Angle(degrees: 22.0))
                     .offset(x: 100.5, y: 68)
+                    .scaleEffect(scale)
+                    .animation(.easeInOut(duration: 1.5), value: scale)
                 
                 GearView()
                     .foregroundStyle(.orange)
                     .frame(width: size, height: size)
                     .rotationEffect(rotationAngle)
+                    .scaleEffect(scale)
                     .gesture(rotationGesture())
+                    .animation(.easeInOut(duration: 1), value: scale)
                 
                 GearView()
                     .foregroundStyle(.yellow)
                     .frame(width: size, height: size)
                     .rotationEffect(-rotationAngle + Angle(degrees: 22.0))
                     .offset(x: -98.5, y: -67)
+                    .scaleEffect(scale)
+                    .animation(.easeInOut(duration: 1.8), value: scale)
                 
                 GearView()
                     .foregroundStyle(.purple)
                     .frame(width: size, height: size)
                     .rotationEffect(rotationAngle)
                     .offset(x: 86, y: -192)
+                    .scaleEffect(scale)
+                    .animation(.easeInOut(duration: 1.3), value: scale)
             }
             
             Text("ROTATE ME")
                 .foregroundStyle(.orange)
                 .font(.headline)
                 .rotationEffect(rotationAngle)
+                .scaleEffect(scale)
+                .animation(.easeInOut(duration: 3), value: scale)
+        }
+        .onAppear {
+            startAnimationWithDelay()
         }
     }
     
@@ -60,6 +76,14 @@ struct ContentView: View {
                 let angle = atan2(dragVector.dy - size / 2, dragVector.dx - size / 2)
                 rotationAngle = Angle(radians: Double(angle))
             }
+    }
+    
+    private func startAnimationWithDelay() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            withAnimation(.easeInOut(duration: 1.0)) {
+                scale = 1.0
+            }
+        }
     }
 }
 
